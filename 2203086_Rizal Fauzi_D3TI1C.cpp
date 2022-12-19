b9d7bd28bd0e291363c773e4a9ec52464ea2d00c
Zalfauzi__ <118164508+Zal77@users.noreply.github.com>
2022-12-19 19:00:47 +0700

    TUGAS TAKE HOME UAS

---
+++ 2203086_Rizal Fauzi_D3TI1C.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+int main()
+{
+    int angka, maks, cari, nilai[100];
+
+    cout<< "Masukkan Banyaknya Angka : ";
+    cin>>angka;
+    for(maks=1; maks<=angka; maks++){
+        cout<<"masukkan angka ke-"<<maks<<" = ";
+        cin>>nilai[maks];
+    }
+
+    cout<< "Hasil Setelah diurutkan : "<<endl;
+    for(maks=1; maks<=angka; maks++){
+        cout<<nilai[maks]<<" | ";
+    }
+    cout<<endl;
+
+    cout<< "Masukkan Angka yang Ingin Dicari : ";
+    cin>>cari;
+    for(maks=1; maks<= angka; maks++){
+        if(nilai[maks]==cari){
+            cout<< "Angka "<<cari<<" didalam indeks ke "<<maks<<endl;
+        }
+    }
+    return 0;
+}
