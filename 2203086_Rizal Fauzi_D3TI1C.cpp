407813ddc4a7a1b3cca809a878498b783589282f
Zalfauzi__ <118164508+Zal77@users.noreply.github.com>
2022-12-19 18:41:01 +0700

    Jawaban Tugas TakeHome UAS

---
+++ 2203086_Rizal Fauzi_D3TI1C.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int angka, maks, cari, awal, nilai[100];
+
+    cout<< "Masukkan Banyaknya Angka : ";
+    cin>>angka;
+    for(maks=1; maks<=angka; maks++){
+        cout<<"masukkan angka ke-"<<maks<<" = ";
+        cin>>nilai[maks];
+    }
+
+    cout<< "Hasil Setelah diurutkan : "<<endl;
+    for(maks=1; maks<=angka; maks++){
+        cout<<nilai[maks]<<" | ";
+    }
+    cout<<endl;
+
+    cout<< "Masukkan Angka yang Ingin Dicari : ";
+    cin>>cari;
+    awal = 0;
+    for(maks=1; maks<= angka; maks++){
+        if(nilai[maks]==cari){
+            awal = 1;
+            cout<< "Angka "<<cari<<" didalam indeks ke "<<maks+1<<endl;
+        }
+    }
+    return 0;
+}
